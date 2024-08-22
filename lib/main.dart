4199e1fbbe6ceab69c9bb65d565d65433e96bfbf
daniel_fu03 <daniel_fu03@outlook.com>
2024-08-22 19:52:15 +0000

    debugging

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  
   runApp(MyApp());
 }
 
--- lib/providers/connectivity_provider.dart
+++ lib/providers/connectivity_provider.dart
@@ -1,25 +1,24 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'package:myapp/constants/constants.dart';
+import 'dart:io' if (dart.library.html) 'dart:html';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ConnectivityProvider with ChangeNotifier {
+  bool isWeb = kIsWeb;
+
   Future<bool> checkInternet() async {
     if (isWeb) {
+      // Aquí podrías implementar una manera de verificar conectividad en web
       return true;
     } else {
       try {
         final result = await InternetAddress.lookup('example.com');
         if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-          //   print('connected');
           return true;
         } else {
-          //    print('not connected');
           return false;
         }
       } on SocketException catch (_) {
-        //    print('not connected');
-
         return false;
       }
     }
@@ -28,9 +27,7 @@ class ConnectivityProvider with ChangeNotifier {
   bool internet = true;
 
   Future<bool> verifyInternet() async {
-    await checkInternet().then((newInternet) {
-      internet = newInternet;
-    });
+    internet = await checkInternet();
     return internet;
   }
 }
